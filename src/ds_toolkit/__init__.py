c43d3f1dd9d4ff720ebcdb5958baa11aaaa2ea56
Aleksandr Shilov <tabbols95@gmail.com>
2026-06-03 20:38:45 +0300

    feat: поля id_columns и date_columns не обязательные

--- src/ds_toolkit/__init__.py
+++ src/ds_toolkit/__init__.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 @dataclass
 class DatasetConfig:
-    id_column: str
-    date_column: str
     target_columns: List[str]
+    id_column: str | None = None
+    date_column: str | None = None
 
 
 class DSToolkit:
@@ -18,38 +18,60 @@ class DSToolkit:
             config: DatasetConfig,
     ):
         self.df = df.copy()
+
         self.id_column = config.id_column
         self.date_column = config.date_column
         self.target_columns = config.target_columns
 
+        self._validate_input()
+
     def _validate_input(self) -> None:
-        if not isinstance(self.df, pd.DataFrame):
-            raise TypeError("df must be a pd.DataFrame")
+        """Validate the input data."""
+        if (
+            self.id_column is not None
+            and self.id_column not in self.df.columns
+        ):
+            raise ValueError(
+                f"id_column='{self.id_column}' not found"
+            )
 
-        if self.date_column not in self.df.columns:
-            raise ValueError("date_column must be in df.columns")
+        if (
+            self.date_column is not None
+            and self.date_column not in self.df.columns
+        ):
+            raise ValueError(
+                f"date_column='{self.date_column}' not found"
+            )
 
-        missing_targets = [
-            col for col in self.target_columns
+        missing_target_columns = [
+            col
+            for col in self.target_columns
             if col not in self.df.columns
         ]
 
-        if missing_targets:
-            raise ValueError(f"target_columns must not contain {missing_targets}")
+        if missing_target_columns:
+            raise ValueError(
+                f"target_columns not found: {missing_target_columns}"
+            )
 
-        if len(self.target_columns) == 0:
-            raise ValueError("target_columns must not be empty")
+        if not self.target_columns:
+            raise ValueError(
+                "target_columns must not be empty"
+            )
 
     @property
     def features(self) -> List[str]:
 
-        excluded = [
-           self.id_column,
-           self.date_column
-       ] + self.target_columns
+        excluded_columns = self.target_columns
+
+        if self.id_column is not None:
+            excluded_columns.append(self.id_column)
+
+        if self.date_column is not None:
+            excluded_columns.append(self.date_column)
 
         return [
             col
             for col in self.df.columns
-            if col not in excluded
+            if col not in excluded_columns
         ]
